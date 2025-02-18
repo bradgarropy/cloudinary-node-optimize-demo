b121e596d3303caa199c1444115c639ab1055443
Brad Garropy <bradgarropy@gmail.com>
2025-02-17 19:59:43 -0600

    done.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import {sayHello} from "./hello"
+import "dotenv/config"
 
-sayHello()
+import {v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({cloud_name: process.env.CLOUD_NAME})
+
+// Quality Options
+// ---------------
+// auto
+// best
+// good
+// eco
+// low
+
+const img = cloudinary.url("cloudinary-node-optimize-demo/porkchop.jpg", {
+    quality: "auto",
+    fetch_format: "auto",
+})
+
+console.log(img)
