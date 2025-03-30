68bd1c82998f8d9278375faba6fd85ae226c96bb
Brad Garropy <bradgarropy@gmail.com>
2025-03-30 13:32:46 -0500

    add format options.

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,13 @@ cloudinary.config({cloud_name: process.env.CLOUD_NAME})
 // eco
 // low
 
+// Format Options
+// ---------------
+// avif
+// jpg
+// png
+// webp
+
 const img = cloudinary.url("cloudinary-node-optimize-demo/porkchop.jpg", {
     quality: "auto",
     fetch_format: "auto",
